4d1f92bb658bf64d4ef234169cc4979dd78514b8
WP Bullet <wpbulletplugins@gmail.com>
2016-05-02 17:17:43 +0200

    Update wpbullet.sh
    update cloudflare

--- wpbullet.sh
+++ wpbullet.sh
@@ -122,30 +122,7 @@ sed -i s"/example.com/${WORDPRESSSITE}/g" /etc/nginx/sites-enabled/wordpress
 install_mariadb
 install_wordpress
 #Fix CloudFlare IP
-cat > /etc/nginx/conf.d/cloudflare.conf<<EOF
-#CloudFlare
-set_real_ip_from   199.27.128.0/21;
-set_real_ip_from   173.245.48.0/20;
-set_real_ip_from   103.21.244.0/22;
-set_real_ip_from   103.22.200.0/22;
-set_real_ip_from   103.31.4.0/22;
-set_real_ip_from   141.101.64.0/18;
-set_real_ip_from   108.162.192.0/18;
-set_real_ip_from   190.93.240.0/20;
-set_real_ip_from   188.114.96.0/20;
-set_real_ip_from   197.234.240.0/22;
-set_real_ip_from   198.41.128.0/17;
-set_real_ip_from   162.158.0.0/15;
-set_real_ip_from   104.16.0.0/12;
-set_real_ip_from   172.64.0.0/13;
-set_real_ip_from   2400:cb00::/32;
-set_real_ip_from   2606:4700::/32;
-set_real_ip_from   2803:f800::/32;
-set_real_ip_from   2405:b500::/32;
-set_real_ip_from   2405:8100::/32;
-#Set the real ip header
-real_ip_header     CF-Connecting-IP;
-EOF
+enable_cloudflare
 service nginx restart
 service php5-fpm restart
 
@@ -230,6 +207,7 @@ install_wordpress
 #chmod 644 /var/www/${WORDPRESSSITE}/wp-config.php
 #Fix CloudFlare IP
 enable_cloudflare
+sed -i s"/CF-Connecting-IP/X-Actual-IP/g" /etc/nginx/conf.d/cloudflare.conf
 service nginx restart
 service php5-fpm restart
 service varnish restart
